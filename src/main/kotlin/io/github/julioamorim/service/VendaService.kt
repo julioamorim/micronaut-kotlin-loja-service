90c79451b3aee55844daaff6b3627b2a6a86405b
julioamorim <julio.l.amorim00@gmail.com>
2021-12-30 03:34:33 -0300

    Implementação do Kafka e consumers

--- src/main/kotlin/io/github/julioamorim/service/VendaService.kt
+++ src/main/kotlin/io/github/julioamorim/service/VendaService.kt
@@ -1,17 +1,23 @@
 package io.github.julioamorim.service
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.github.julioamorim.DTO.input.VendaInput
 import io.github.julioamorim.DTO.output.Parcela
 import io.github.julioamorim.DTO.output.Venda
+import io.github.julioamorim.producer.VendaProducer
 import jakarta.inject.Singleton
 import java.time.LocalDate
+import java.util.*
+import kotlin.collections.ArrayList
 
 @Singleton
 class VendaService(
-    private val veiculoService: VeiculoService
+    private val veiculoService: VeiculoService,
+    private val vendaProducer: VendaProducer,
+    private val objectMapper: ObjectMapper
 ) {
 
-    fun realizarVenda(vendaInput: VendaInput) {
+    fun realizarVenda(vendaInput: VendaInput): Venda {
         val veiculo = veiculoService.getVeiculo(vendaInput.veiculo)
         var parcelas: List<Parcela> = ArrayList<Parcela>()
         var valorParcela = vendaInput.valor.divide(vendaInput.quantidadeParcelas.toBigDecimal())
@@ -26,5 +32,13 @@ class VendaService(
         var venda = Venda(vendaInput.cliente, veiculo, vendaInput.valor, parcelas)
 
         print(venda)
+        confirmarVenda(venda)
+
+        return venda
+    }
+
+    fun confirmarVenda(venda: Venda) {
+        var vendaJSON = objectMapper.writeValueAsString(venda)
+        vendaProducer.publicarVenda(UUID.randomUUID().toString(), vendaJSON)
     }
 }
